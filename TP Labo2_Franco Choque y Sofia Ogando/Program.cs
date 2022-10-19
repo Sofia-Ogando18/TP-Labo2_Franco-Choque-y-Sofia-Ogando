e44ee657cb78c27433f6a80c06801ff1118d7bb7
Sofia Ogando <84402232+Sofia-Ogando18@users.noreply.github.com>
2022-10-19 15:24:28 -0300

    Agregar archivos de proyecto.

---
+++ TP Labo2_Franco Choque y Sofia Ogando/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_Labo2_Franco_Choque_y_Sofia_Ogando
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
