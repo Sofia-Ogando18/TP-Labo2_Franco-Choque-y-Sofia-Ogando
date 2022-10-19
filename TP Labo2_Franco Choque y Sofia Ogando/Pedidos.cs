d805f6024f1eb21d9f1a0812990791fc8a7e5ba0
Sofia Ogando <84402232+Sofia-Ogando18@users.noreply.github.com>
2022-10-19 16:12:03 -0300

    clase pedido

---
+++ TP Labo2_Franco Choque y Sofia Ogando/Pedidos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Labo2_Franco_Choque_y_Sofia_Ogando
+{
+    internal class Pedidos
+    {
+        //para hacer una lista con los pedidos
+        // se me ocurrio hacer 3 listas en el main con los articulos y hacer una cuarta lista con los pedidos
+        // que tenga los datos del cliente (direccion,DNI , nombre y tipo de entrega) y ademas uns pila (o algo asi) con las cosas que compro
+        //osea en esta clase hacer una pila (no se si se puede). si se te ocurre algo mejor
+
+        public string Nombre_Apellido;
+        public string DNI;
+        public int hora_de_entrega=0;
+
+
+    }
+}
