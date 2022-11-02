a1ff5b29d90efe4ae6c91e16fad40fc4e827517f
Sofia Ogando <84402232+Sofia-Ogando18@users.noreply.github.com>
2022-11-01 21:55:22 -0300

    lista de elctrodomesticos

---
+++ TP Labo2_Franco Choque y Sofia Ogando/Listas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Labo2_Franco_Choque_y_Sofia_Ogando
+{
+    internal class Listas
+    {
+        public List<string> Listado;
+        public int cant;
+
+        public Listas() { 
+            Listado = new List<string>();
+            cant = 0;    
+        }
+
+    }
+}
--- TP Labo2_Franco Choque y Sofia Ogando/Program.cs
+++ TP Labo2_Franco Choque y Sofia Ogando/Program.cs
@@ -11,9 +11,28 @@ namespace TP_Labo2_Franco_Choque_y_Sofia_Ogando
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
+        /// 
+         static void Agregar_elementos(List<string> lista_,Listas l) {
+
+            lista_.Add("licuadora");
+            lista_.Add("exprimidor");
+            lista_.Add("molinillos de cafe");
+            lista_.Add("tostadoras");
+            lista_.Add("cafeteras");
+            lista_.Add("extractor de jugos");
+            l.cant = 6;
+        
+         }
+
         [STAThread]
         static void Main()
         {
+            Listas Pequenios_electrodomesticos=new Listas();
+            Agregar_elementos(Pequenios_electrodomesticos.Listado,Pequenios_electrodomesticos);
+
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
