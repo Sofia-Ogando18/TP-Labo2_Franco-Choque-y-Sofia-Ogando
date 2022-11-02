a8f954ded78764e4180faec79005ce287c7dc49a
Sofia Ogando <84402232+Sofia-Ogando18@users.noreply.github.com>
2022-11-01 22:03:09 -0300

    listado con productos

--- TP Labo2_Franco Choque y Sofia Ogando/Program.cs
+++ TP Labo2_Franco Choque y Sofia Ogando/Program.cs
@@ -16,21 +16,53 @@ namespace TP_Labo2_Franco_Choque_y_Sofia_Ogando
 
             lista_.Add("licuadora");
             lista_.Add("exprimidor");
-            lista_.Add("molinillos de cafe");
-            lista_.Add("tostadoras");
-            lista_.Add("cafeteras");
+            lista_.Add("molinillo de cafe");
+            lista_.Add("tostadora");
+            lista_.Add("cafetera");
             lista_.Add("extractor de jugos");
             l.cant = 6;
         
          }
+        static void Agregar_elementos_blancos(List<string> lista_, Listas l)
+        {
+
+            lista_.Add("cocina");
+            lista_.Add("calefone");
+            lista_.Add("termotanque");
+            lista_.Add("lavarropas");
+            lista_.Add("secarropas");
+            lista_.Add("heladera");
+            lista_.Add("microondas");
+            lista_.Add("freezers");
+            l.cant = 8;
+
+        }
+
+        static void Agregar_elementos_Elect(List<string> lista_, Listas l)
+        {
+
+            lista_.Add("notebook");
+            lista_.Add("impresora");
+            lista_.Add("monitor");
+            lista_.Add("mouse");
+            lista_.Add("parlantes");
+            lista_.Add("teclados");
+            lista_.Add("televisor");
+            l.cant = 7;
+
+        }
+
+        
 
         [STAThread]
         static void Main()
         {
             Listas Pequenios_electrodomesticos=new Listas();
             Agregar_elementos(Pequenios_electrodomesticos.Listado,Pequenios_electrodomesticos);
-
-
+            Listas Linea_blanca=new Listas();
+            Agregar_elementos_blancos(Linea_blanca.Listado, Linea_blanca);
+            Listas Electronicos = new Listas();
+            Agregar_elementos_Elect(Electronicos.Listado, Electronicos);
 
 
             Application.EnableVisualStyles();
